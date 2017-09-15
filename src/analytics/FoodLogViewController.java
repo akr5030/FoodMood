4b0424b7e0e56e2ab4d52925b6859eae17d53b27
Jennifer Mullen <jsm158@psu.edu>
2017-09-14 19:02:37 -0700

    Created empty analytics views and controllers

---
+++ src/analytics/FoodLogViewController.java
@@ -0,0 +1,22 @@
+package analytics;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author jsm
+ */
+public class FoodLogViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
