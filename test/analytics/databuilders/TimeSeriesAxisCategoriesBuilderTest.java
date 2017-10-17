9a911e814437bb595c530ae82edf8af3df4aa803
Jennifer Mullen <redjen@gmail.com>
2017-10-17 14:30:43 -0700

    Completed unit tests for TimeSeriesAxisCategoriesBuilder

--- test/analytics/databuilders/TimeSeriesAxisCategoriesBuilderTest.java
+++ test/analytics/databuilders/TimeSeriesAxisCategoriesBuilderTest.java
@@ -1,22 +1,37 @@
 package analytics.databuilders;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import javafx.collections.ObservableList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
- * @author redjen
+ * @author jsm158
+ * @since M03-A04
  */
 public class TimeSeriesAxisCategoriesBuilderTest {
+
+    TimeSeriesAxisCategoriesBuilder tsbDefault;
+    TimeSeriesAxisCategoriesBuilder tsbParams;
+
+    private final static LocalDate EXPECTED_START_DATE = LocalDate.of(2017, Month.JANUARY, 2);
+    private final static LocalDate EXPECTED_END_DATE = LocalDate.of(2017, Month.JANUARY, 7);
+    private final static DateTimeFormatter EXPECTED_DATE_FORMAT = DateTimeFormatter.ofPattern("MMM d, u");
 
     public TimeSeriesAxisCategoriesBuilderTest() {
     }
 
     @Before
     public void setUp() {
+
+        tsbDefault = new TimeSeriesAxisCategoriesBuilder();
+        tsbParams = new TimeSeriesAxisCategoriesBuilder(EXPECTED_START_DATE,
+                EXPECTED_END_DATE, EXPECTED_DATE_FORMAT);
     }
 
     @After
@@ -24,36 +39,67 @@ public class TimeSeriesAxisCategoriesBuilderTest {
     }
 
     @Test
-    public void testBuildCategories() {
+    public void testGetStartDateDefault() {
+        assertEquals(LocalDate.now().minusDays(7), tsbDefault.getStartDate());
     }
 
     @Test
-    public void testGetStartDate() {
+    public void testGetStartDateParams() {
+        assertEquals(EXPECTED_START_DATE, tsbParams.getStartDate());
     }
 
     @Test
     public void testSetStartDate() {
+        tsbParams.setStartDate(LocalDate.of(2016, Month.MARCH, 3));
+        assertEquals(LocalDate.of(2016, Month.MARCH, 3), tsbDefault.getStartDate());
     }
 
     @Test
-    public void testGetEndDate() {
+    public void testGetEndDateDefault() {
+        assertEquals(LocalDate.now(), tsbDefault.getEndDate());
+    }
+
+    @Test
+    public void testGetEndDateParams() {
+        assertEquals(EXPECTED_END_DATE, tsbParams.getEndDate());
     }
 
     @Test
     public void testSetEndDate() {
+        tsbParams.setEndDate(LocalDate.of(2017, Month.DECEMBER, 5));
+        assertEquals(LocalDate.of(2017, Month.DECEMBER, 5), tsbParams.getEndDate());
     }
 
     @Test
-    public void testGetDateFormat() {
+    public void testGetDateFormatDefault() {
+        assertEquals(DateTimeFormatter.ISO_DATE, tsbDefault.getDateFormat());
+    }
+
+    @Test
+    public void testGetDateFormatParams() {
+        assertEquals(EXPECTED_DATE_FORMAT, tsbParams.getDateFormat());
     }
 
     @Test
     public void testSetDateFormat() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM u");
+        System.out.println(dtf);
+        tsbParams.setDateFormat(dtf);
+        assertEquals(dtf, tsbParams.getDateFormat());
     }
 
     @Test
-    public void testBuildChain() {
-
+    public void testBuildChain() throws DataBuilderException {
+        String dateString = "Jan %d, 2017";
+        ObservableList<String> series = tsbParams.buildCategories();
+        assertEquals(6, series.size());
+        series.forEach(c -> System.out.println(c));
+        assertEquals(String.format(dateString, 2), series.get(0));
+        assertEquals(String.format(dateString, 3), series.get(1));
+        assertEquals(String.format(dateString, 4), series.get(2));
+        assertEquals(String.format(dateString, 5), series.get(3));
+        assertEquals(String.format(dateString, 6), series.get(4));
+        assertEquals(String.format(dateString, 7), series.get(5));
     }
 
     /**
