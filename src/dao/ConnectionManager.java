caa0fe393ca9dc3c4d1d807da2a53d5ac8c85cdd
Jennifer Mullen <redjen@gmail.com>
2017-10-18 18:57:17 -0700

    Constants for file locations

--- src/dao/ConnectionManager.java
+++ src/dao/ConnectionManager.java
@@ -1,33 +1,24 @@
 package dao;
 
-import java.sql.Connection;
-
 /**
  * Provides a connection to the application database.
+ *
+ * We chose to use files for storage, so this class returns the location of
+ * files, directories, and paths.
  *
  * @author jsm158
  *
  */
 public class ConnectionManager {
 
-    private static final String HOST = "";
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    public static final String DATA_DIR = "data";
+    public static final String ACCOUNT_DATA_DIR = "accountData";
+    public static final String MOOD_RECORD_FILE = "moodRecords.txt";
 
     /**
      * Constructs a new ConnectionManager
      */
     public ConnectionManager() {
-    }
-
-    /**
-     * Returns a connection to the application database
-     *
-     * @return a database connection
-     * @throws dao.DaoException if there is an error connecting to the database
-     */
-    public Connection getConnection() throws DaoException {
-        return null;
     }
 
 }
