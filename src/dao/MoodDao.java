4d17bd2d952a2fc378e69aadce82403bd49ee314
mzc5387 <mzc5387@gmail.com>
2017-10-20 21:10:28 -0400

    Updated MoodDAO to return list of moods, and get mood by Id.
    Updated MoodDAO to return list of moods, and get mood by Id.

--- src/dao/MoodDao.java
+++ src/dao/MoodDao.java
@@ -1,7 +1,11 @@
 package dao;
 
 import foodmood.Mood;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Provides methods for reading mood objects from the database.
@@ -15,6 +19,12 @@ import java.util.ArrayList;
 public class MoodDao {
     
     private final ConnectionManager cm;
+    private static final String COMMA_DELIMITER = ",";
+    //private static final String NEW_LINE = "\n";
+    //private static final String FILE_HEADER = "Id,MoodName";
+
+    private static final int MOOD_ID_INDEX = 0;
+    private static final int MOOD_NAME_INDEX = 1;
 
     /**
      * Constructs a new MoodDao
@@ -31,7 +41,41 @@ public class MoodDao {
      * executing the query
      */
     public ArrayList<Mood> getAllMoods() throws DaoException {
-        return null;
+        BufferedReader reader = null;
+        try {
+            ArrayList<Mood> moods = new ArrayList<Mood>();
+            String ln = "";
+
+            reader = new BufferedReader(new FileReader("data/Moods.csv"));
+            reader.readLine();
+
+            while ((ln = reader.readLine()) != null) {
+                String[] obj = ln.split(COMMA_DELIMITER);
+                if (obj.length > 0) {
+                    Mood md = new Mood(Integer.parseInt(obj[MOOD_ID_INDEX]), obj[MOOD_NAME_INDEX]);
+                    moods.add(md);
+                }
+
+                //Print Moods
+//                for (Mood md : moods) {
+//                    System.out.println(md.toString());
+//                }
+
+            }
+            return moods;
+            //TestHarness.getInstance().testMoodControllerRead(moods);
+
+        } catch (Exception e) {
+            System.out.println("Error in reading CSV file");
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                System.out.println("Error closing fileReader");
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -45,6 +89,34 @@ public class MoodDao {
      * executing the query
      */
     public Mood getMoodById(int moodId) throws DaoException {
-        return null;
+        BufferedReader reader = null;
+        Mood rtnValue = null;
+        try {
+            String ln = "";
+            reader = new BufferedReader(new FileReader("data/Moods.csv"));
+            reader.readLine();
+
+            while ((ln = reader.readLine()) != null) {
+                String[] obj = ln.split(COMMA_DELIMITER);
+                if (obj.length > 0) {
+                    Mood md = new Mood(Integer.parseInt(obj[MOOD_ID_INDEX]), obj[MOOD_NAME_INDEX]);
+                    if(md.getId() == moodId){
+                        rtnValue = md;
+                    }
+                }
+            }
+            return rtnValue;
+        } catch (Exception e) {
+            System.out.println("Error in reading CSV file");
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                System.out.println("Error closing fileReader");
+                e.printStackTrace();
+            }
+        }
     }
+    
 }
