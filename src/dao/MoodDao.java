d6042b1266a05f9f139a9e97176c436694c4d589
mzc5387 <mzc5387@users.noreply.github.com>
2017-10-22 15:00:18 -0400

    Updated MoodDao w/Tests (#21)
    * Updated MoodDAO to return list of moods, and get mood by Id.
    
    Updated MoodDAO to return list of moods, and get mood by Id.
    
    * Fixed bugs in moodDao
    
    Fixed bugs in moodDao
    
    * Updated MoodDao
    
    Updated MoodDao w/ Tests

--- src/dao/MoodDao.java
+++ src/dao/MoodDao.java
@@ -1,7 +1,9 @@
 package dao;
 
 import foodmood.Mood;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Provides methods for reading mood objects from the database.
@@ -9,18 +11,18 @@ import java.util.ArrayList;
  * Write functionality is not provided because our requirements do not allow the
  * creation of new moods. However, this could be added in the future.
  *
- * @author jsm158
+ * @author matt cano
  *
  */
 public class MoodDao {
-    
-    private final ConnectionManager cm;
+
+    ArrayList<Mood> m = new ArrayList<Mood>();
 
     /**
      * Constructs a new MoodDao
      */
-    public MoodDao() {
-        this.cm = new ConnectionManager();
+    public MoodDao() throws DaoException {
+        m = createMoods();
     }
 
     /**
@@ -31,20 +33,55 @@ public class MoodDao {
      * executing the query
      */
     public ArrayList<Mood> getAllMoods() throws DaoException {
-        return null;
+        return m;
     }
 
     /**
      * Returns the mood object with the specified ID
-     * 
+     *
      * This method returns null if there is no mood with the specified ID
-     * 
+     *
      * @param moodId the mood ID
      * @return the specified mood or null if there is no match
-     * @throws dao.DaoException if there is an error connecting to the database or
-     * executing the query
+     * @throws dao.DaoException if there is an error connecting to the database
+     * or executing the query
      */
     public Mood getMoodById(int moodId) throws DaoException {
-        return null;
+        Mood rtnValue = null;
+        try {
+            for (Mood md : m) {
+                if (md.getId() == moodId) {
+                    rtnValue = md;
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error in reading CSV file");
+            //e.printStackTrace();
+        }
+        return rtnValue;
+    }
+
+    public ArrayList<Mood> createMoods() throws DaoException {
+        Mood m = new Mood(1, "Excited");
+        Mood m1 = new Mood(2, "Happy");
+        Mood m2 = new Mood(3, "Meh");
+        Mood m3 = new Mood(4, "Sad");
+        Mood m4 = new Mood(5, "Angry");
+
+        ArrayList<Mood> moods = new ArrayList<Mood>();
+
+        try {
+            moods.add(m);
+            moods.add(m1);
+            moods.add(m2);
+            moods.add(m3);
+            moods.add(m4);
+
+        } catch (Exception ex) {
+            System.out.println("Error creating moods....");
+//            e.printStackTrace();
+        }
+
+        return moods;
     }
 }
