102055f07284108879e605cc0df5d7aaf3e3658c
akr5030 <akr5030@psu.edu>
2017-10-22 20:25:03 -0400

    Alyson's individual assignment (#26)

---
+++ src/foodmood/MealFactory.java
@@ -0,0 +1,19 @@
+package foodmood;
+
+/**
+ *
+ * @author aswecker
+ */
+public class MealFactory implements Meal {
+
+    @Override
+    public void mealType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void food() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
