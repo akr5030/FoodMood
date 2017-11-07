72b90aea5ae295b2c49983ad8c1ac039844494bb
akr5030 <akr5030@psu.edu>
2017-11-07 11:29:13 -0500

    Update FoodRecordViewController.java

--- src/foodmood/FoodRecordViewController.java
+++ src/foodmood/FoodRecordViewController.java
@@ -1,14 +1,8 @@
 package foodmood;
 
-import dao.DaoException;
-import dao.FoodRecordDao;
 import java.net.URL;
-import java.time.LocalDate;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.Initializable;
-import testHarness.TestHarness;
 
 /**
  * FXML Controller class
@@ -19,6 +13,8 @@ public class FoodRecordViewController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url the url (path) to the associated FXML document
+     * @param rb the application's resource bundle
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -28,7 +24,9 @@ public class FoodRecordViewController implements Initializable {
 //        TestHarness.getInstance().changeScene("/analytics/FoodLogView.fxml");
         
     }    
-    
+    /**
+     * Allows the user to add a food record.
+     */
     public void addFoodRecord() {
 //        FoodRecord fr = new FoodRecord(1, 1, LocalDate.of(2017, 1, 1), "taco");
 //        FoodRecordDao dao = new FoodRecordDao();
