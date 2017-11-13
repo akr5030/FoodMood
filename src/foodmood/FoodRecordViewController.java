0416f089233bc4be3cf7b7225353407cfc531aa6
akr5030 <akr5030@psu.edu>
2017-11-12 21:25:32 -0500

    Account Files

--- src/foodmood/FoodRecordViewController.java
+++ src/foodmood/FoodRecordViewController.java
@@ -1,15 +1,35 @@
 package foodmood;
 
+import dao.FoodDao;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
 /**
  * FXML Controller class
  *
  * @author redjen
  */
+
+
+        
 public class FoodRecordViewController implements Initializable {
+
+    private FoodDao f;
+@FXML
+private ListView foodListView;
+    
+
+    
+    
+    //private final ArrayList foods = new ArrayList();
+    
+    
 
     /**
      * Initializes the controller class.
@@ -18,12 +38,24 @@ public class FoodRecordViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        //ListView<String> list = new ListView<>();
+        
+ObservableList<String> items = FXCollections.observableArrayList("Milk","Pizza");        
+
+    
+  
+    
+         
+            
+    foodListView.setItems( items);
+    }
 
 //        addFoodRecord();
 //        TestHarness.getInstance().testFoodRecordControllerAdd();
 //        TestHarness.getInstance().changeScene("/analytics/FoodLogView.fxml");
         
-    }    
+        
     /**
      * Allows the user to add a food record.
      */
