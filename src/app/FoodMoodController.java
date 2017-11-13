f65457a828a2b19bc61eb302502a8e2c689c34ca
akr5030 <akr5030@psu.edu>
2017-11-12 21:24:11 -0500

    Account Files

--- src/app/FoodMoodController.java
+++ src/app/FoodMoodController.java
@@ -1,10 +1,22 @@
 package app;
 
+import dao.DaoException;
+import dao.FoodDao;
+import foodmood.Food;
+import foodmood.FoodRecord;
+import foodmood.MoodRecord;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 /**
  * Main controller for the application
@@ -14,6 +26,16 @@ public class FoodMoodController implements Initializable {
 
     @FXML
     private Label label;
+    
+  
+    
+    //private final ObservableList<String> food = FXCollections.observableArrayList();
+    
+    //private ArrayList<FoodDao> food;
+    
+    
+    
+    
 
     /**
      * Initializes the controller class.
@@ -23,7 +45,14 @@ public class FoodMoodController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        
+        
+        
+        
     }
+    
+   
 
+    
 }
